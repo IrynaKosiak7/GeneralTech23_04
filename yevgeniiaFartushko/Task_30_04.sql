38fd72361aa58777eb264ea3002663bdacf67547
Yevgeniia Fartushko <yevgeshkaf@gmail.com>
2024-04-30 13:47:16 +0200

    гзвupdate file

--- yevgeniiaFartushko/Task_30_04.sql
+++ yevgeniiaFartushko/Task_30_04.sql
@@ -1,15 +1,20 @@
-create database social_network
+-- создание бд social_network
+create database social_network;
 
+
+-- создание таблицы users
 create table users (
-id int primary key auto_increment,
-created_at timestamp,
-username varchar(64)
+    id int primary key auto_increment,
+    created_at timestamp,
+    username varchar(64)
 );
 
+
+-- создание таблицы posts
 create table posts (
-id int primary key auto_increment,
-created_at timestamp,
-user_id int,
-title varchar(128),
-body varchar(1280)
+    id int primary key auto_increment,
+    created_at timestamp,
+    user_id int,
+    title varchar(128),
+    body varchar(1280)
 );
