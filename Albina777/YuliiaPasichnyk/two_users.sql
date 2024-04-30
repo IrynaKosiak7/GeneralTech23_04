ab6e3365fbc7fe8afeea0be25c441f7f437d9dea
Julia <pasichnyk.julia2@gmail.com>
2024-04-30 14:22:20 +0200

    task 2

---
+++ Albina777/YuliiaPasichnyk/two_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (created_at, fullname, email, country)
+VALUES
+  (current_timestamp(), 'Anna Petrowa', 'anna@example.org', 'Germany'),
+  (current_timestamp(), 'Yuliia Pasichnyk', 'pas@example.org', 'Germany')
