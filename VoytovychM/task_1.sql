87df617731e6a62f3f1f43d12083b122a04bbab7
VoytovychM <151627487+VoytovychM@users.noreply.github.com>
2024-04-30 14:02:59 +0200

    Update task_1.sql
    add corrections

--- VoytovychM/task_1.sql
+++ VoytovychM/task_1.sql
@@ -1,3 +1,6 @@
+//  create DB social_network
 create database social_network;
+// create Table users
 create table users(user_id int primary key auto_increment, created_at timestamp, name varchar(128), email varchar(128), country varchar(64), is_blocked bool);
+// create Table posts
 create table posts(post_id int primary key auto_increment, created_at timestamp, title varchar(128), body text(128), user_id int);
