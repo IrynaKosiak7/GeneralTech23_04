bdb4c7ee57fe0e45918fd2eca75087124ae68b8a
Trinity <alles.aug.2000@gmail.com>
2024-04-30 14:22:24 +0200

    update

---
+++ Tatiana/2Users.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (created_at, fullname, email, country, is_blocked)
+VALUES
+	(current_timestamp(), 'User1', 'ivan50@example.org', 'Germany', False),
+  (current_timestamp(), 'User 2', 'ivan22@example.org', 'Germany', False)
