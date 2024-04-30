df9d3253173eb70d81d2df4e3a8e95db8acd1a8d
Albina Khamitova <albina757@yahoo.com>
2024-04-30 13:53:28 +0200

    updated task8

--- Albina777/task8.sql
+++ Albina777/task8.sql
@@ -1,15 +1,20 @@
-create database social_network
-create table users (
-id int,
-created_at timestamp,
-date_of_birth timestamp,
-email varchar(64)
-);
-use social_network;
+CREATE DATABASE social_network;
 
-create table publications (
-id int,
-user_id int,
-created_at timestamp,
-public_text varchar(64)
+
+CREATE TABLE users (
+    id int,
+    fullname varchar(128),
+    created_at timestamp,
+    date_of_birth timestamp,
+    email varchar(64)
+);
+
+
+USE social_network;
+
+CREATE TABLE  publications (
+    id int,
+    user_id int,
+    created_at timestamp,
+    public_text varchar(64)
 );
