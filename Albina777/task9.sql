503ad6db3d273c69d229becf86a3cf767ceca913
Albina Khamitova <albina757@yahoo.com>
2024-04-30 15:09:06 +0200

    updated task10

---
+++ Albina777/task9.sql
@@ -0,0 +1,6 @@
+INSERT INTO users (created_at, email, date_of_birth)
+VALUES
+
+	(current_timestamp(), 'susan@example.org', '1990-04-25'),
+	(current_timestamp(), 'mike656@example.org', '1990-07-18');
+	
