665212701afd9fc25f179b33936066928ac7954f
Iryna Kosiakovska <ira.oleinick2013@gmail.com>
2024-05-02 10:28:49 +0200

    updated social network

--- irynaKosiakovska/two_users.sql
+++ irynaKosiakovska/two_users.sql
@@ -1,4 +1,4 @@
 INSERT INTO users (name, date_of_birth, country, phone_number, email, is_deleted)
 VALUES
-	(current_timestamp(), 'Ivan Ivanov', 24/09/1986, 'Ukraine', 096874356924, 'ivan@example.org', False),
-	(current_timestamp(), 'OLena Ivanovich', 18/12/1932, 'Germany', 096874355974, 'olena@example.org', False);
+	('Ivan Ivanov', 24/09/1986, 'Ukraine', 096874356924, 'ivan@example.org', False),
+	('OLena Ivanovich', 18/12/1932, 'Germany', 096874355974, 'olena@example.org', False);
